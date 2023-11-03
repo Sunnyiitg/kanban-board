ae5e0c91fa4654c82baa9a199413d1cbbf287518
Sunny Kumar <107452199+Sunnyiitg@users.noreply.github.com>
2023-11-03 05:56:53 +0530

    Update Dashboard.js
    removed local storage stuff

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState} from 'react'
+import React, { useContext, useEffect} from 'react'
 import './Dashboard.css'
 import { AppContext } from '../../context/AppContext';
 import FetchData from '../../components/FetchData/FetchData';
@@ -10,80 +10,78 @@ import UserTitle from '../Options/UserTitle/UserTitle';
 import PriorTitle from '../Options/PriorTitle/PriorTitle';
 const Dashboard = () => {
     const {statusgroup,usergroup,prioritygroup,priorityorder,titleorder} = useContext(AppContext);
-    const [groupStatus, setGroupStatus] = useState("");
-    const [orderStatus, setOrderStatus] = useState("");
-  
-    useEffect(() => {
-      const groupData = localStorage.getItem('groupData');
-      if (groupData) {
-        const parsedGroupData = JSON.parse(groupData);
-        for (const key in parsedGroupData) {
-          if (Object.hasOwnProperty.call(parsedGroupData, key)) {
-            const value = parsedGroupData[key];
-            if (value === true) {
-              setGroupStatus(key);
-              break;
-            }
-          }
-        }
-      }
-      const orderData = localStorage.getItem('orderData');
-      if (orderData) {
-        const parsedOrderData = JSON.parse(orderData);
-        for (const key in parsedOrderData) {
-          if (Object.hasOwnProperty.call(parsedOrderData, key)) {
-            const value = parsedOrderData[key];
-            if (value === true) {
-              setOrderStatus(key);
-              break;
-            }
-          }
-        }
-      }
-    });
-    console.log('Group Status:', groupStatus);
-    console.log('Order Status:', orderStatus);
+    // let groupStatus="";
+    // let orderStatus="";
+    // useEffect(() => {
+    //     const orderData = localStorage.getItem('orderData');
+    //     const groupData = localStorage.getItem('groupData');
+    //     if (groupData) {
+    //         const parsedGroupData = JSON.parse(groupData);
+    //         for (const key in parsedGroupData) {
+    //           if (Object.hasOwnProperty.call(parsedGroupData, key)) {
+    //             const value = parsedGroupData[key];
+    //             if (value === true) {
+    //               groupStatus = key;
+    //               break;
+    //             }
+    //           }
+    //         }
+    //     }
+        
+    //     if (orderData) {
+    //         const parsedOrderData = JSON.parse(orderData);
+    //         for (const key in parsedOrderData) {
+    //           if (Object.hasOwnProperty.call(parsedOrderData, key)) {
+    //             const value = parsedOrderData[key];
+    //             if (value === true) {
+    //               orderStatus = key;
+    //               break;
+    //             }
+    //           }
+    //         }
+    //     }
+    //   });
     return (
         <div className='dashboard-main'>
             <FetchData/>
             <div className="dashboard-wrapper">
                 <div className="dashboard-sp dashboard-col">
-                {((statusgroup === true && priorityorder === true) || (groupStatus === "status" && orderStatus === "prior")) && (
-                    <div className='statusandpriority'>
-                    <StatusPrior />
-                    </div>
-                )}
+                    {statusgroup === true && priorityorder === true && (
+                        <div className='statusandpriority'>
+                            <StatusPrior/>
+                        </div>
+                    )}
                 </div>
                 <div className="dashboard-st dashboard-col">
-                    {((statusgroup === true && titleorder === true) || (groupStatus==="status" && orderStatus ==="title")) && (
+                    {statusgroup === true && titleorder === true && (
                         <div className='statusandtitle'>
                             <StatusTitle/>
                         </div>
                     )}
                 </div>
                 <div className="dashboard-up dashboard-col">
-                    {((usergroup === true && priorityorder === true) || (groupStatus === "user" && orderStatus==="prior")) && (
+                    {usergroup === true && priorityorder === true && (
                         <div className='userandpriority'>
                             <UserPrior/>
                         </div>
                     )}
                 </div>
                 <div className="dashboard-ut dashboard-col">
-                    {((usergroup === true && titleorder === true) || (groupStatus==="user" && orderStatus === "title")) && (
+                    {usergroup === true && titleorder === true && (
                         <div className='userandtitle'>
                             <UserTitle/>
                         </div>
                     )}
                 </div>
                 <div className="dashboard-pp dashboard-col">
-                    {((prioritygroup === true && priorityorder === true) || (groupStatus==="prior" && orderStatus==="prior")) && (
+                    {prioritygroup === true && priorityorder === true && (
                         <div className='priorityandpriority'>
                             <PriorPrior/>
                         </div>
                     )}
                 </div>
                 <div className="dashboard-pt dashboard-col">
-                    {((prioritygroup === true && titleorder === true) || (groupStatus === "prior" && orderStatus === "title")) && (
+                    {prioritygroup === true && titleorder === true && (
                         <div className='priorityandtitle'>
                             <PriorTitle/>
                         </div>
